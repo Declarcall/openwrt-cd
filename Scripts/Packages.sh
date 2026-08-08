a85dc690f68bf8db00775669ab00765fec8d439e
Declarcall <Declarcall@gmail.com>
2026-08-09 04:30:13 +0800

    feat(store): add linkease/istore app store plugin

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -83,6 +83,7 @@ UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "timecontrol" "sirpdboy/luci-app-timecontrol" "main"
 UPDATE_PACKAGE "filetransfer" "sirpdboy/luci-app-filetransfer" "main"
 UPDATE_PACKAGE "ttyd" "sirpdboy/luci-app-ttyd" "main"
+UPDATE_PACKAGE "store" "linkease/istore" "main" "pkg"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "axonhub gecoosac sing-box luci-app-homeproxy luci-app-timewol luci-app-wolplus luci-app-wolultra"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
